aec3b569e0fa96ee1c3d10ed773ee7b716925cc1
Jirasit <48820052+TONG2K@users.noreply.github.com>
2023-09-04 14:49:36 +0700

    update putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,38 +6,40 @@
 /*   By: jikarunw <jikarunw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 15:40:01 by jikarunw          #+#    #+#             */
-/*   Updated: 2023/09/03 15:42:24 by jikarunw         ###   ########.fr       */
+/*   Updated: 2023/09/04 14:49:01 by jikarunw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_abs(int nbr)
+{
+	if (nbr < 0)
+		return (-nbr);
+	else
+		return (nbr);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	rev;
-	int	digit;
+	char	str[13];
+	int		is_neg;
+	int		length;
 
+	is_neg = (n < 0);
+	ft_bzero(str, 13);
 	if (n == 0)
+		str[0] = '0';
+	length = 0;
+	while (n != 0)
 	{
-		ft_putchar_fd('0', fd);
-		return ;
+		str[length++] = '0' + ft_abs(n % 10);
+		n = (n / 10);
 	}
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	rev = 0;
-	while (n > 0)
-	{
-		digit = n % 10;
-		rev = rev * 10 + digit;
-		n /= 10;
-	}
-	while (rev > 0)
-	{
-		digit = rev % 10 + '0';
-		ft_putchar_fd(digit, fd);
-		rev /= 10;
-	}
+	if (is_neg)
+		str[length] = '-';
+	else if (length > 0)
+		length--;
+	while (length >= 0)
+		write(fd, &str[length--], 1);
 }
