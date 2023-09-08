b25df9e28731f6d8d6b300f030a7321f4553a0fd
Jirasit <48820052+TONG2K@users.noreply.github.com>
2023-09-08 13:05:38 +0700

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jikarunw <jikarunw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 22:20:35 by jikarunw          #+#    #+#             */
-/*   Updated: 2023/09/06 14:30:46 by jikarunw         ###   ########.fr       */
+/*   Updated: 2023/09/08 13:04:18 by jikarunw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ int	ft_atoi(const char *str)
 	i = 0;
 	if (!str)
 		return (0);
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\b' || str[i] == '\f'
-		|| str[i] == '\r' || str[i] == ' ' || str[i] == '\v')
+	while ((str[i] >= '9' && str[i] <= '13') || str[i] == ' ')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
@@ -33,14 +32,7 @@ int	ft_atoi(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (res > INT_MAX / 10 || (res == INT_MAX 
-				/ 10 && (str[i] - '0') > INT_MAX % 10))
-		{
-			if (neg_digit == -1)
-				return INT_MIN; // Overflow, return INT_MIN
-			else
-				return INT_MAX; // Overflow, return INT_MAX
-		}
+		res *= 10;
 		res = res * 10 + (str[i++] - '0');
 	}
 	return (res * neg_digit);
